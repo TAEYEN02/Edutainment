eca67127cb6b1d81da6002c82ea6fc90b5bfb208
[TAEYEN02] <[xodus2331@naver.com]>
2023-11-13 13:17:38 +0900

    진행상황 프론트앤드 작업

--- app/src/main/java/org/koreait/edutainment/ProgressActivity.java
+++ app/src/main/java/org/koreait/edutainment/ProgressActivity.java
@@ -3,6 +3,7 @@ package org.koreait.edutainment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 public class ProgressActivity extends AppCompatActivity {
 
@@ -10,6 +11,11 @@ public class ProgressActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_progress);
+
+        ProgressBar progressBar = findViewById(R.id.progressBar);
+
+// 진행 상황 설정 (0-100)
+        progressBar.setProgress(50);
     }
 
 }
